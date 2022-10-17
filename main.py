7d3d61db1f3ccb9b06cd14da74793f9ba7126801
Abhijit Datta Chowdhury <abhijitdc@abhijits-mbp.lan>
2022-10-17 13:20:04 +1100

    arg parser

--- main.py
+++ main.py
@@ -4,21 +4,36 @@ from os import environ
 from faker import Faker
 from jinja2 import Environment, PackageLoader, select_autoescape
 from jinja2.runtime import Context
+import argparse
+from google.cloud import storage
+
+#[START template read]
+def read_file(bucketname, blobname):
+    print('Reading the full file contents:\n')
+    client = storage.Client()
+
+    bucket = client.get_bucket(bucketname)
+    blob = bucket.get_blob(blobname)
+    contents = blob.download_as_string()
+    print(contents)
+    return contents
+#[END template read]
 
 def main():
 
-    # env = jinja2.Environment()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--template', required=True, help='name of the template file')
+    parser.add_argument('--output', help='pubsub, gcs')
+    parser.add_argument('--rate', type=int, default=100, help='req/sec e.g. 100')
+    args = parser.parse_args()
+
     env = Environment(
     loader=PackageLoader("main"),
     autoescape=select_autoescape()
     )
-    # template = env.from_string("Hello {{ name }}")
-    template = env.get_template("mytemplate.html")
+    template = env.get_template(args.template)
 
-    # ctx = Context(environment=env, parent=None, name="main", blocks={})
     fake = Faker()
-    # print(fake.name())
-    # print(fake.random.randint(10,50))
     rs = template.render(fake=fake)
     print(rs)
 
